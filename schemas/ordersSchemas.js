4bd5638f8455cc68a0d71c1bdcbbbf278327a35b
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-11-02 15:52:29 +0200

    Update ordersSchemas.js

--- schemas/ordersSchemas.js
+++ schemas/ordersSchemas.js
@@ -3,7 +3,14 @@ import Joi from "joi";
 export const createOrderSchema = Joi.object({
   userName: Joi.string().required(),
   userAddress: Joi.string().required(),
-  products: Joi.array().items(Joi.string()).required(),
+  products: Joi.array()
+    .items(
+      Joi.object({
+        productId: Joi.string().required(),
+        quantity: Joi.number().required(),
+      })
+    )
+    .required(),
   totalSum: Joi.number().required(),
   status: Joi.string().default("pending"),
 });
